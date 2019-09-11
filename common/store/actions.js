a6a349c2b2238fb9caaefb752533bfb91c147602
Taz-Dingo <943533080@gmail.com>
2019-09-11 18:11:02 +0800

    11

--- common/store/actions.js
+++ common/store/actions.js
@@ -2,14 +2,14 @@ const loadUserInfo = ({commit,state}, userInfo) => {
 	return new Promise((resolve, reject) => {
 			 if(global.getToken()&&state.userInfo){
 				 setTimeout(function(){
-					 resolve(state.userInfo);
+					 resolve(state.userInfo)
 				 },10);
 			 }
 			 else{
 				 global.http.auth("user_info").then(res=>{
 						if (res.errcode === 0) {
-							commit('setUserInfo', res.data)
-							resolve(res.data)
+							commit('setUserInfo', res.data.data)
+							resolve(res.data.data)
 						} else {
 							resolve()
 						}
